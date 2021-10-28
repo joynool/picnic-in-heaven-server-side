662b698acb9f6b6b104e224598e18f31eb130334
JoynoolAbedinMithoon <mithun55@gmail.com>
2021-10-29 01:53:43 +0600

    database connected

--- index.js
+++ index.js
@@ -9,11 +9,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run ()
 {
     try {
+        await client.connect();
+        const database = client.db('picnic_in_heaven');
+        const serviceCollection = database.collection('service');
+        const orderCollection = database.collection('order');
 
     }
     finally {
