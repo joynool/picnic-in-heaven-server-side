0e8d905d9b6b013be0dd04131bbd15b8e0c1b970
JoynoolAbedinMithoon <mithun55@gmail.com>
2021-10-31 00:25:12 +0600

    update order data bug fixes

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ async function run ()
         });
 
         //UPDATE order status
-        app.put('order/:id', async (req, res) =>
+        app.put('/order/:id', async (req, res) =>
         {
             const id = req.params.id;
             const updateStatus = req.body;
